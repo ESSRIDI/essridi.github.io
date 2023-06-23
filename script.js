8f1ac8d0c801e1f898fe8c2010f86a8b733afab7
ESSRIDI <kamel.essridi.pro@gmail.com>
2023-06-23 11:52:21 +0200

    first publish

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log("ok");
+
+
+//#region Dynamic resume Tabs
+const tabsResumeSectionTarget = document.querySelector(
+  "#wrapper-tabs-section-resume"
+);
+const resumeTabs = tabsResumeSectionTarget.querySelectorAll('[role="tab"]');
+const resumeTabsContent = tabsResumeSectionTarget.querySelectorAll('[role="tabpanel"]');
+
+tabsResumeSectionTarget.onclick = function(e)  {
+  const id = e.target.id;
+  console.log(id);
+  if (id) {
+    resumeTabs.forEach((tab) => {
+      tab.classList.remove("active");
+      tab.ariaSelected =false;
+    });
+    e.target.classList.add("active");
+  
+e.target.ariaSelected =true;
+resumeTabsContent.forEach((content) => {
+  content.classList.remove("show", "active");
+  
+ 
+});
+    const element = document.getElementById(id+"s");
+    element.classList.add("show", "active");
+    
+  }
+};
+
